176eb66e7cdb9f5da60a3511a8a8dbd675400c51
Jayvrat Dwivedi <jayvratdwivedi202@gmail.com>
2025-07-17 11:01:36 +0530

    added mobile menu and transition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import { motion } from "framer-motion";
-import { FiGithub, FiTwitter, FiLinkedin } from "react-icons/fi";
+import { FiGithub, FiTwitter, FiLinkedin, FiMenu, FiX } from "react-icons/fi";
+import { useState } from "react";
+import { a } from "framer-motion/client";
 
 const Header = () => {
+  //Toggle the Menu Open/Close
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
   return (
     <header className="absolute w-full z-50 transition-all duration-300">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 md:h-20">
@@ -77,23 +82,83 @@ const Header = () => {
           >
             <FiGithub className="w-5 h-5" />
           </motion.a>
+          {/* Hire Me */}
+          <motion.button
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              delay: 1.6,
+              duration: 0.8,
+              type: "spring",
+              stiffness: 100,
+              damping: 15,
+            }}
+            className="ml-4 px-4 py-2 rounded-xl bg-gradient-to-r from-gray-400 to-gray-100 text-violet-700 font-bold  hover:from-violet-700 hover:to-purple-700 hover:text-white transition-all duration-500"
+          >
+            Hire Me!
+          </motion.button>
         </div>
-        {/* Hire Me */}
-        <motion.button
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            delay: 1.6,
-            duration: 0.8,
-            type: "spring",
-            stiffness: 100,
-            damping: 15,
-          }}
-          className="ml-4 px-4 py-2 rounded-xl bg-gradient-to-r from-gray-400 to-gray-100 text-violet-700 font-bold  hover:from-violet-700 hover:to-purple-700 hover:text-white transition-all duration-500"
-        >
-          Hire Me!
-        </motion.button>
+        {/* Mobile Menu Button */}
+        <div className="md:hidden flex items-center">
+          <motion.button
+            whileTap={{ scale: 0.7 }}
+            onClick={toggleMenu}
+            className="text-gray-300"
+          >
+            {isOpen ? (
+              <FiX className="h-6 w-6" />
+            ) : (
+              <FiMenu className="h-6 w-6" />
+            )}
+          </motion.button>
+        </div>
       </div>
+      {/* Mobile Menu */}
+      <motion.div
+        initial={{ opacity: 0, height: 0 }}
+        animate={{
+          opacity: isOpen ? 1 : 0,
+          height: isOpen ? "auto" : 0,
+        }}
+        transition={{ duration: 0.5 }}
+        className="md:hidden overflow:hidden bg-white dark:bg-gray-900 shadow-lg px-4 py-5 space-y-5 "
+      >
+        <nav className="flex flex-col space-y-3">
+          {["Home", "About", "Projects", "Experience", "Contact"].map(
+            (item) => (
+              <a
+                onClick={toggleMenu}
+                className="text-gray-300 font-medium py-2"
+                key={item}
+                href="#"
+              >
+                {item}
+              </a>
+            )
+          )}
+        </nav>
+        <div className="pt-4 border-t border-gray-200 dark:border-gray-700">
+          <div className="flex space-x-5">
+            <a href="#">
+              <FiTwitter className="h-5 w-5 text-gray-300" />
+            </a>
+            <a href="#">
+              <FiLinkedin className="h-5 w-5 text-gray-300" />
+            </a>
+            <a href="#">
+              <FiGithub className="h-5 w-5 text-gray-300" />
+            </a>
+          </div>
+          <button
+            onClick={() => {
+              toggleMenu();
+            }}
+            className="mt-4 block w-full px-4 py-2 rounded-lg bg-gradient-to-r from-violet-600 to-violet-400 font-bold"
+          >
+            Contact Me
+          </button>
+        </div>
+      </motion.div>
     </header>
   );
 };
