90f2b0ee95fad78201aa9026020ce584926209bc
Jayvrat Dwivedi <jayvratdwivedi202@gmail.com>
2025-07-17 10:27:38 +0530

    add hire me button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
             Jayvrat Dwivedi
           </span>
         </motion.div>
-        {/*Desktop Navigation*/}
+        {/* Desktop Navigation */}
         <nav className="lg:flex hidden space-x-8">
           {["Home", "About", "Projects", "Experience", "Contact"].map(
             (item, index) => (
@@ -48,7 +48,7 @@ const Header = () => {
             )
           )}
         </nav>
-        {/*Social icons - Desktop */}
+        {/* Social icons - Desktop */}
         <div className="md:flex hidden items-center space-x-4">
           <motion.a
             initial={{ opacity: 0, scale: 0.5 }}
@@ -78,6 +78,21 @@ const Header = () => {
             <FiGithub className="w-5 h-5" />
           </motion.a>
         </div>
+        {/* Hire Me */}
+        <motion.button
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            delay: 1.6,
+            duration: 0.8,
+            type: "spring",
+            stiffness: 100,
+            damping: 15,
+          }}
+          className="ml-4 px-4 py-2 rounded-xl bg-gradient-to-r from-gray-400 to-gray-100 text-violet-700 font-bold  hover:from-violet-700 hover:to-purple-700 hover:text-white transition-all duration-500"
+        >
+          Hire Me!
+        </motion.button>
       </div>
     </header>
   );
